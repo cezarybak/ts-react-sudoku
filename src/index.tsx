f1f2d1f1960a150388c811d3ab1d35bdd147dfa0
Cezary Bąk <cezarybak@MBP-Cezary.home>
2021-03-23 21:00:00 +0100

    styled components

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { ThemeProvider } from 'styled-components'
 
-import {reportWebVitals} from './core';
+import { Card, Content, Grid, Title } from './components'
+import { unregister } from './core'
+import { GlobalStyles, theme } from './styles'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <div>Hello World</div>
-  </React.StrictMode>,
+  <ThemeProvider theme={theme}>
+    <GlobalStyles />
+    <Content data-cy="content">
+      <Title data-cy="title">Sudoku</Title>
+      <Card data-cy="card">
+        <Grid />
+      </Card>
+    </Content>
+  </ThemeProvider>,
   document.getElementById('root')
-);
+)
 
-
-reportWebVitals();
+unregister()
